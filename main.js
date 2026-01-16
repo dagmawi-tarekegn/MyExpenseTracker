093c1bc61ecd166dc273513fded870a8139cf8b1
Amen Sime <amensime@gmail.com>
2026-01-16 23:03:04 +0300

    adding function and buttonn listener

--- main.js
+++ main.js
@@ -8,3 +8,31 @@ const incomeEl = document.getElementById("income");
 const expenseEl = document.getElementById("expense");
 const balanceEl = document.getElementById("balance");
 const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", addTransaction);
+
+function addTransaction() {
+    const title = titleInput.value;
+    const amount = amountInput.value;
+    const type = typeSelect.value;
+
+    if (title === "" || amount === "") {
+        alert("You Forgot To Fill All The Fields");
+        return;
+    }
+
+    const transaction = {
+        id: Date.now(),
+        title,
+        amount: Number(amount),
+        type
+    };
+
+    transactions.push(transaction);
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+
+    titleInput.value = "";
+    amountInput.value = "";
+
+    
+}
