559e4a26fbebdc7c15f167463193794bb5f20f70
Amen Sime <amensime@gmail.com>
2026-01-16 22:57:22 +0300

    store inputs

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+const titleInput = document.getElementById("title");
+const amountInput = document.getElementById("amount");
+const typeSelect = document.getElementById("type");
+const list = document.getElementById("list");
+const incomeEl = document.getElementById("income");
+const expenseEl = document.getElementById("expense");
+const balanceEl = document.getElementById("balance");
+const addBtn = document.getElementById("addBtn");
